ed28a3c554cd219a670c11ff9f43a912250fe325
matheus-1618 <matheus1207live@gmail.com>
2023-05-29 11:28:29 -0300

    Add colab

--- data/setup.bat
+++ data/setup.bat
@@ -5,7 +5,7 @@ set url=http://data.gharchive.org/
 set ext=.json.gz
 
 for /L %%y in (2015,1,2015) do (
-  for %%m in (01,02) do (
+  for %%m in (01,02,03) do (
     for /L %%d in (1,1,31) do (
       set day=0%%d
       set day=!day:~-2!
